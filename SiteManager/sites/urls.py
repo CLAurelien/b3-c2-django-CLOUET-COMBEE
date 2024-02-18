d3561873a56b7d5da4ddbc6b9f63ac80c6dd6d2f
Aurel <aurelien010603@gmail.com>
2024-02-18 19:52:45 +0100

    Possibilité d'ajouter un site + ajout d'un header pour naviguer et d'un accueil

--- SiteManager/sites/urls.py
+++ SiteManager/sites/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.list, name='list'),
+    path('add/', views.add_site, name='add')
 ]
--- SiteManager/sites/views.py
+++ SiteManager/sites/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Sites
+from .forms import SiteForm
 from django.http import HttpResponse
 
 
+
 def index(request):
+    return render(request, 'index.html')
+
+
+def list(request):
     sites = Sites.objects.all()
     return render(request, 'liste_sites.html', {'sites': sites})
+
+
+def add_site(request):
+    if request.method == 'POST':
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            site = form.save()
+            return redirect('/list')
+    else:
+        form = SiteForm()
+
+    return render(request, 'add_site.html', {'form': form})
