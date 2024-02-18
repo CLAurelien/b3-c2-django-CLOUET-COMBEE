bc4e26e826ca6b45dedf192c48ffc3733fbefabf
Aurélien <aurelien010603@gmail.com>
2024-02-18 16:44:09 +0100

    Création superadmin + améliorations models/html (avec migration)

--- SiteManager/sites/models.py
+++ SiteManager/sites/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Sites(models.Model):
-    name = models.CharField(max_length=40),
-    address = models.CharField(max_length=100),
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=100, blank=True)
+    id_email = models.CharField(max_length=60)
     password = models.CharField(max_length=30)
